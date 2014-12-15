1e142d9d871a5aa2ef8888f50965dac0dfcfb88c
Mario Lamacchia <mariolamacchia@gmail.com>
2014-12-15 15:05:23 +0100

    Translate date

--- www/js/filters.js
+++ www/js/filters.js
@@ -5,3 +5,19 @@ angular.module('starter.filters', [])
         return /\(([^)]+)\)/.exec(author)[1];
     }
 })
+
+.filter('dateFormat', function() {
+    return function(dateString) {
+        var itMonths = [
+            'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno',
+            'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'
+        ];
+        var itDays = ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab'];
+        var d = new Date(dateString);
+        return itDays[d.getDay()] + ', ' +
+            d.getDate() + ' ' +
+            itMonths[d.getMonth()] + ' ' +
+            d.getFullYear() + ' - ' +
+            d.toLocaleTimeString();
+    }
+});
